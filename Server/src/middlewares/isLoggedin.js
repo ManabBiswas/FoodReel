dace9fb541fc527516ed88834dce7c7c9b782f4a
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-09-10 23:31:18 +0530

    middleware added

---
+++ Server/src/middlewares/isLoggedin.js
@@ -0,0 +1,17 @@
+import foodPartnerModel from "../models/foodPartner.Model";
+import jwt from 'jsonwebtoken';
+
+async function isLoggedin(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+       return res.status(401).json({ message: "login first" });
+    };
+    try{
+        const decoded =jwt.verify(tocken, process.env.JWT_SECRET);
+        const foodPartner = await foodPartnerModel.findById(decoded.id);
+        req.foodPartner = foodPartner;
+        next();
+    }catch(error){
+        res.status(401).json({ message: "Unauthorized" });
+    }
+}
