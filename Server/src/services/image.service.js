2340f8697326647bf84ce1603b236d2b75049109
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-09-11 17:23:12 +0530

    imagekit setup done

---
+++ Server/src/services/image.service.js
@@ -0,0 +1,17 @@
+import ImageKit from "imagekit";
+
+const imagekit = new ImageKit({
+    publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    urlEndpoint : process.env.IMAGEKIT_URL
+});
+
+async function uploadImage(file, fileName) {
+    const response = await imagekit.upload({
+        file,
+        fileName
+    });
+    return response.url;
+}
+
+export default uploadImage;
