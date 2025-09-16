1a8d5bea1322b8456381b874e0967d805a6ef0a0
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-09-16 15:37:40 +0530

    home

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <p>This is the home page</p>
+      <nav>
+        <li></li>
+      </nav>
+
+    </div>
   )
 }
 
