03c52be3a03c3d49a393ece7c322c02ce69de043
Manab Biswas <manabbiswas108108+github@gmail.com>
2025-09-20 19:27:51 +0530

    navbar file created!

---
+++ Client/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <div>logo</div>
+            <div>links</div>
+            <div>buttons</div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
